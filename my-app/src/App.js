530874c36f440a1699004c3bbd53f712c5334bb0
Ramez Bariekzahy <ramez@seznam.cz>
2023-12-19 15:53:13 +0100

    MySharedButton added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
 import { useState } from 'react';
 
+
+
+const name = "Dev";
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+const listItems = products.map(product =>
+  <li key={product.id} style={{ color: product.isFruit ? 'blue' : 'orange' }}>
+    {product.title}
+  </li>
+);
+
+
+
 function MyButton() {
   function handleClick() {
     setCount(count + 1);
@@ -21,13 +37,30 @@ function MyNameButton(props) {
   return (<button className="myBtn" onClick={() => { alert('You clicked ' + props.myName) }}>I'm a button {props.myName}</button>);
 }
 
+function MySharedButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+
   return (
     <div>
       <div>
         <h1>Welcome to my {name} app</h1>
         <MyButton />
         <MyNameButton myName="Ramez" />
+        <MySharedButton count={count} onClick={handleClick} />
+        <MySharedButton count={count} onClick={handleClick} />
       </div>
       <br />
       <div>
@@ -36,16 +69,3 @@ export default function App() {
     </div>
   );
 }
-
-
-const name = "Tereza";
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
-const listItems = products.map(product =>
-  <li key={product.id} style={{ color: product.isFruit ? 'blue' : 'orange' }}>
-    {product.title}
-  </li>
-);
