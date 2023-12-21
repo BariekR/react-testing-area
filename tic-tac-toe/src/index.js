82ebebaea524e4344a9bf98c299b4d544bfc2556
Ramez Bariekzahy <ramez@seznam.cz>
2023-12-21 22:35:59 +0100

    ProductApp added.

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,10 +2,16 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./App";
+import ProductApp from "./ProductApp";
 
 const root = createRoot(document.getElementById("root"));
-root.render(
+/*root.render(
   <StrictMode>
     <App />
+  </StrictMode>
+);*/
+root.render(
+  <StrictMode>
+    <ProductApp />
   </StrictMode>
 );
